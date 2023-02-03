7f6f5ed1bbc19d4e382c8b34178f2fa9325fe132
Ale Mendoza <alemendoza1990@gmail.com>
2023-02-03 20:19:44 -0300

    Cambios de prueba en index.php

--- pages/index.php
+++ pages/index.php
@@ -19,5 +19,19 @@
     </ul>
     </div>
     </div> <!-- esto es un comentario -->
+
+    <div class="container pt-4 pb-5">
+    <div class="row pb-5">
+    <div class="col-8 mx-auto">
+    <h3><i class="fas fa-user-shield"></i> Tus datos registrados son:</h3>
+    <ul>
+    <li>Nombre: <?php echo $_SESSION['users'];?>
+    <li>Apellido: <?php echo$_SESSION['userSurname'];?></li>
+    <li>Email registrado: <?php echo$_SESSION['userEmail'];?></li>
+    <li>Teléfono registrado: <?php echo $_SESSION['userPhoneNumber'];?></li>
+    <li>Sos miembro desde: <?php echo$_SESSION['userStartDate'];?></li>
+    </ul>
+    </div>
+    </div> <!-- esto es un comentario -->
 </div>
 </section>
